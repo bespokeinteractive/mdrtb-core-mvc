fdd7cd60068fc48ba1af19243e758d4132680bfa
Dennys Henry <daugmaud@gmail.com>
2019-02-06 20:40:35 +0300

    Return no records found. Enter activates search

--- wwwroot/js/home-index-script.js
+++ wwwroot/js/home-index-script.js
@@ -8,11 +8,16 @@
         else {
             window.location.href = "/patients/search?q=" + searchString;
         }
-
     }); 
 
     jq('li.collection-item.dismissable p.contacts').click(function(){
         window.location.href = jq(this).data('url');
     });
 
+    jq('#search_string').keypress(function (e) {
+        var key = e.which;
+        if(key == 13) {
+            jq('a.btn-search').click();;
+        }
+   });
 });
