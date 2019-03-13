a78bf6f0bf6ec04e3abe3bc58ced6e9c465cd7f1
Dennys Henry <daugmaud@gmail.com>
2019-03-13 15:22:43 +0300

    Fixed Bug in Recently added Date

--- ViewModel/RegisterViewModel.cs
+++ ViewModel/RegisterViewModel.cs
@@ -12,8 +12,14 @@ namespace EtbSomalia.ViewModel
         public List<SelectListItem> Facilities { get; set; }
         public Facility Active { get; set; }
 
+        public DateTime Date1x { get; set; }
+        public DateTime Date2x { get; set; }
+
         public RegisterViewModel() {
             Type = "tb";
+
+            Date1x = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+            Date2x = Date1x.AddMonths(1).AddDays(-1);
 
             Register = new List<Register>();
             Facilities = new List<SelectListItem>();
